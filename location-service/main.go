c38a71d16374639d2575ad08ad01a86798ef65d6
MSkrzypietz <michael.skr97@gmail.com>
2023-10-03 19:28:57 +0200

    Add location service with search api handler

---
+++ location-service/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+type NearbySearchRequest struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Radius    float64 `json:"radius,omitempty"`
+}
+
+func main() {
+	router := httprouter.New()
+	router.POST("/v1/search/nearby", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		var search NearbySearchRequest
+		decoder := json.NewDecoder(req.Body)
+		decoder.DisallowUnknownFields()
+		err := decoder.Decode(&search)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			fmt.Println(err)
+			return
+		}
+		res.Write([]byte("Unimplemented"))
+	})
+	log.Fatal(http.ListenAndServe(":3001", router))
+}
